ee139390304ae9d7f5bf47fbdbca0d92a5f5146a
alu0100886870 <alu0100886870@ull.edu.es>
2016-02-25 03:03:44 +0000

    Arreglando gh-pages

--- temperature.js
+++ temperature.js
@@ -2,15 +2,20 @@
 function calculate() {
   var result;
   var temp = original.value;
-  var regexp = /([-+]?\d+(?:\.\d*)?)\s*([fFcC])/;
-  
+  var regexp = /^(\s*[-+]?\d+(?:\.\d*)?(?:e[+-]?\d+)?)\s*((f(a(r(e(n(h(e(i(t)?)?)?)?)?)?)?)?)|((c(e(l(s(i(u(s)?)?)?)?)?)?)))\s*$/i;
+
   var m = temp.match(regexp);
-  
+
   if (m) {
     var num = m[1];
     var type = m[2];
     num = parseFloat(num);
-    if (type == 'c' || type == 'C') {
+
+     for (var i = 0; i < m.length; i++) {
+       //alert(m[i]);
+    }
+
+    if (type[0] == 'c' || type[0] == 'C') {
       result = (num * 9/5)+32;
       result = result.toFixed(1)+" Farenheit"
     }
@@ -19,8 +24,10 @@ function calculate() {
       result = result.toFixed(1)+" Celsius"
     }
     converted.innerHTML = result;
+    converted.className = "converted";
   }
   else {
     converted.innerHTML = "ERROR! Try something like '-4.2C' instead";
+    converted.className = "error";
   }
 }
